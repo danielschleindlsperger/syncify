33818d09091475a62363f8ae877391d4bbd673eb
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2020-04-13 13:45:32 +0200

    add ethical analytics script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,10 +6,26 @@ import { Navbar } from '../components/nav-bar'
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+      <ArnoldAnalytics />
+    </>
   )
 }
 
 export default App
+
+const ArnoldAnalytics = () => (
+  <>
+    <script
+      src="https://app.usearnold.com/assets/pixel.min.js"
+      data-arnold-analytics="f6b56035bb5b7e2da72778b6e76ee780"
+      data-arnold-analytics-location="off"
+    ></script>
+    <noscript>
+      <img src="https://app.usearnold.com/hello?key=f6b56035bb5b7e2da72778b6e76ee780" alt="" />
+    </noscript>
+  </>
+)
