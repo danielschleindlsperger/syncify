b8c9ee40664d1d0910842245d66dd4ae42cb13b3
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2020-04-26 09:58:40 +0200

    load room data client side

--- src/pages/rooms/[id].tsx
+++ src/pages/rooms/[id].tsx
@@ -1,27 +1,46 @@
 import React from 'react'
-import { GetServerSideProps } from 'next'
 import Link from 'next/link'
 import Head from 'next/head'
-import axios from 'axios'
+import { useRouter } from 'next/router'
 import cx from 'classnames'
 import { dropWhile } from 'ramda'
-import { ServerResponse } from 'http'
 import { Playlist } from '../../components/playlist'
-import { Room, PlaylistTrack } from '../../types'
-import { AppUrl } from '../../config'
-import { isAxiosError } from '../../utils/errors'
 import { Chat } from '../../components/chat'
 import { Player } from '../../components/player'
 import { SpotifyPlayerProvider, withPlayerStore } from '../../components/spotify-player'
 import { Navbar } from '../../components/nav-bar'
-import { AuthenticatedOnly } from '../../components/auth'
 import { ShareButton } from '../../components/share-button'
 
+type Room = import('../../types').Room
+type PlaylistTrack = import('../../types').PlaylistTrack
 type Playlist = import('../../types').Playlist
 
 type RoomProps = { room: Room }
 
-export default withPlayerStore(({ room }: RoomProps) => {
+// room container
+export default withPlayerStore(() => {
+  const router = useRouter()
+  const { id } = router.query
+  if (Array.isArray(id)) throw new Error('id must be a string!')
+  const req = useRoom(id)
+
+  if (req.state === 'loading') {
+    return <div>Loading room data...</div>
+  }
+
+  if (req.state === 'error') {
+    if (req.errorType === 'unauthenticated') {
+      // TODO: Login modal
+      return <div>You are unauthenticated</div>
+    }
+    // TODO: Error page
+    return <div>Whoopps, something bad happened!</div>
+  }
+
+  return <Room room={req.data} />
+})
+
+const Room = ({ room }: RoomProps) => {
   const { name, playlist } = room
 
   const remainingTracks = dropPlayedTracks(playlist)
@@ -31,34 +50,32 @@ export default withPlayerStore(({ room }: RoomProps) => {
       <Head>
         <title key="title">{room.name} - Syncify</title>
       </Head>
-      <AuthenticatedOnly>
-        <Navbar>
-          <Link href="/rooms">
-            <a className="inline-block bg-gray-100 text-gray-600 px-3 py-1 rounded-sm">
-              {'< Join a different room'}
-            </a>
-          </Link>
-        </Navbar>
-        <div className="mt-16 px-8 max-w-5xl mx-auto">
-          {remainingTracks.length > 0 ? (
-            <>
-              <div className="flex items-end">
-                <h1 className="text-4xl font-bold">{name}</h1>
-                <ShareButton className="flex-grow-0 ml-auto" />
-              </div>
+      <Navbar>
+        <Link href="/rooms">
+          <a className="inline-block bg-gray-100 text-gray-600 px-3 py-1 rounded-sm">
+            {'< Join a different room'}
+          </a>
+        </Link>
+      </Navbar>
+      <div className="mt-16 px-8 max-w-5xl mx-auto">
+        {remainingTracks.length > 0 ? (
+          <>
+            <div className="flex items-end">
+              <h1 className="text-4xl font-bold">{name}</h1>
+              <ShareButton className="flex-grow-0 ml-auto" />
+            </div>
 
-              <Chat roomId={room.id} className="mt-8" />
-              <Playlist playlist={playlist} className="mt-8" />
-              <Player />
-            </>
-          ) : (
-            <PlaylistIsOver className="mt-8" />
-          )}
-        </div>
-      </AuthenticatedOnly>
+            <Chat roomId={room.id} className="mt-8" />
+            <Playlist playlist={playlist} className="mt-8" />
+            <Player />
+          </>
+        ) : (
+          <PlaylistIsOver className="mt-8" />
+        )}
+      </div>
     </SpotifyPlayerProvider>
   )
-})
+}
 
 const PlaylistIsOver = ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => (
   <div className={cx(className, 'mt-8')} {...props}>
@@ -83,6 +100,39 @@ const PlaylistIsOver = ({ className, ...props }: React.HTMLAttributes<HTMLElemen
   </div>
 )
 
+// example implementation, maybe we could make a wrapper that handles all cases generically
+type ApiErrorType = 'unauthenticated' | 'server-error' | 'not-found'
+
+type RoomLoadingState =
+  | {
+      state: 'loading'
+      data: undefined
+    }
+  | { state: 'error'; errorType: ApiErrorType; message?: string; data: undefined }
+  | { state: 'loaded'; data: Room }
+
+const useRoom = (id: string | undefined): RoomLoadingState => {
+  const [state, setState] = React.useState<RoomLoadingState>({ state: 'loading', data: undefined })
+  React.useEffect(() => {
+    if (!id) return
+    window.fetch(`/api/rooms/${id}`, { credentials: 'include' }).then((res) => {
+      const { status } = res
+      if (status === 401) {
+        return setState({ state: 'error', errorType: 'unauthenticated', data: undefined })
+      }
+      if (status >= 500) {
+        return setState({ state: 'error', errorType: 'server-error', data: undefined })
+      }
+      if (status === 200) {
+        return res.json().then((data) => setState({ state: 'loaded', data }))
+      }
+      throw new Error(`Unexpected response status code: ${status}`)
+    })
+  }, [id])
+
+  return state
+}
+
 const dropPlayedTracks = (playlist: Playlist): PlaylistTrack[] => {
   let offset = Date.now() - Date.parse(playlist.createdAt)
   return dropWhile((t) => {
@@ -93,41 +143,4 @@ const dropPlayedTracks = (playlist: Playlist): PlaylistTrack[] => {
     }
     return false
   }, playlist.tracks)
-}
-
-export const getServerSideProps: GetServerSideProps<RoomProps> = async (ctx) => {
-  const { params } = ctx
-  const id = params && typeof params.id === 'string' ? params.id : undefined
-
-  if (!id) throw new Error(`Could not find a room for id ${id}`)
-  // forward client's cookies for access control
-  const Cookie = ctx.req.headers.cookie
-
-  if (!Cookie) {
-    redirectToLogin(ctx.res)
-    return (null as unknown) as { props: RoomProps }
-  }
-
-  try {
-    const room = await axios
-      .get<Room>(`${AppUrl}/api/rooms/${id}`, { headers: { Cookie } })
-      .then((x) => x.data)
-
-    return { props: { room } }
-  } catch (e) {
-    if (isAxiosError(e)) {
-      if (e.response?.status === 401) {
-        redirectToLogin(ctx.res)
-        return (null as unknown) as { props: RoomProps }
-      }
-      throw e
-    }
-    throw e
-  }
-}
-
-const redirectToLogin = (res: ServerResponse) => {
-  res.statusCode = 307
-  res.setHeader('Location', '/api/auth/login')
-  res.end()
 }
