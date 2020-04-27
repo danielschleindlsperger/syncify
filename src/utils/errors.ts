2171c180d3e5af603d737725afa5d65bb97d3383
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2020-04-27 20:51:37 +0200

    remove now unused dependency axios

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,9 +1,3 @@
-import { AxiosError } from 'axios'
-
-export const isAxiosError = (x: any): x is AxiosError => {
-  return x && x.isAxiosError === true
-}
-
 export class UnreachableError extends Error {
   constructor(value: never, message = `Unreachable case: ${value}`) {
     super(message)
