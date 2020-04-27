89cd98557d0305594caa5041f471c8c3905f54dc
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2020-04-27 20:43:39 +0200

    read env file for local builds to allow for development without vercel teams account

--- next.config.js
+++ next.config.js
@@ -1,13 +1,24 @@
-module.exports = {
-  env: {
-    JWT_SECRET: process.env.JWT_SECRET,
-    APP_URL: process.env.APP_URL,
-    DATABASE_URL: process.env.DATABASE_URL,
-    SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
-    SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
-    SPOTIFY_REDIRECT_URL: process.env.SPOTIFY_REDIRECT_URL,
-    PUSHER_APP_ID: process.env.PUSHER_APP_ID,
-    PUSHER_APP_KEY: process.env.PUSHER_APP_KEY,
-    PUSHER_SECRET: process.env.PUSHER_SECRET,
-  },
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+const dotenv = require('dotenv')
+
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    require('dotenv').config({ path: '.env.build' })
+  }
+
+  return {
+    ...defaultConfig,
+    env: {
+      ...defaultConfig.env,
+      JWT_SECRET: process.env.JWT_SECRET,
+      APP_URL: process.env.APP_URL,
+      DATABASE_URL: process.env.DATABASE_URL,
+      SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+      SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
+      SPOTIFY_REDIRECT_URL: process.env.SPOTIFY_REDIRECT_URL,
+      PUSHER_APP_ID: process.env.PUSHER_APP_ID,
+      PUSHER_APP_KEY: process.env.PUSHER_APP_KEY,
+      PUSHER_SECRET: process.env.PUSHER_SECRET,
+    },
+  }
 }
