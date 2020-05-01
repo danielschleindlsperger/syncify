aa81bf8267f1ccc4daa28ba0138feb8edd36917f
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2020-05-01 16:33:04 +0200

    remove env helper that exploded client and storybook builds

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
-export const AppUrl = env(process.env.APP_URL, 'APP_URL')
+export const AppUrl = process.env.APP_URL!
 
 export const SpotifyConfig = {
-  clientId: env(process.env.SPOTIFY_CLIENT_ID, 'SPOTIFY_CLIENT_ID'),
-  clientSecret: env(process.env.SPOTIFY_CLIENT_SECRET, 'SPOTIFY_CLIENT_SECRET'),
-  redirectUri: env(process.env.SPOTIFY_REDIRECT_URL, 'SPOTIFY_REDIRECT_URL'),
+  clientId: process.env.SPOTIFY_CLIENT_ID!,
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
+  redirectUri: process.env.SPOTIFY_REDIRECT_URL!,
 }
 
 // the scopes requested from the user upon login
@@ -13,8 +13,3 @@ export const SpotifyScopes = [
   'user-read-private',
   'playlist-read-private',
 ]
-
-function env(envVar: string | undefined, name: string): string {
-  if (!envVar) throw new Error(`Environment variable "${name}" is not defined`)
-  return envVar
-}
