d04ee531eca5711e16a692297764a6f7021b491d
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2020-05-02 17:33:06 +0200

    align the volume slider vertically to make it more obvious that it is in fact not a progress bar

--- src/components/player.tsx
+++ src/components/player.tsx
@@ -14,16 +14,20 @@ export const Player = (props: React.HTMLAttributes<HTMLElement>) => {
   const { duration, position } = playbackState
   const { name, album, artists } = currentTrack
   const coverArt = findLargestImage(album.images)?.url
-  const byline = artists.map(a => a.name).join(', ')
+  const byline = artists.map((a) => a.name).join(', ')
 
   return (
     <div className="fixed bottom-0 w-full max-w-3xl flex justify-start bg-white" {...props}>
       <img src={coverArt} alt={`album cover: ${name} by ${byline}`} className="w-40 h-40" />
-      <div className="flex-grow flex flex-col justify-center px-4 py-2">
-        <span className="text-xl text-gray-800 font-bold leading-tight">{name}</span>
-        <span className="text-gray-800 font-semibold mt-2">{byline}</span>
-        {isPlaying && <Progress className="w-full mt-2" duration={duration} position={position} />}
-        <VolumeSlider className="mt-2" />
+      <div className="flex flex-grow ml-4 mb-4">
+        <div className="flex-grow flex flex-col justify-start">
+          <span className="text-xl text-gray-800 font-bold leading-tight">{name}</span>
+          <span className="text-gray-800 font-semibold mt-2">{byline}</span>
+          {isPlaying && (
+            <Progress className="w-full mt-auto" duration={duration} position={position} />
+          )}
+        </div>
+        <VolumeSlider className="ml-auto" />
       </div>
     </div>
   )
--- src/components/volume-controls.tsx
+++ src/components/volume-controls.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import cx from 'classnames'
 import { useSpotifyPlayer } from './spotify-player'
 
-type VolumeSliderProps = React.HTMLAttributes<HTMLElement>
+declare module 'react' {
+  interface InputHTMLAttributes<T> extends HTMLAttributes<T> {
+    // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/range#orient
+    orient?: 'horizontal' | 'vertical'
+  }
+}
 
-export const VolumeSlider = (props: VolumeSliderProps) => {
+type VolumeSliderProps = React.InputHTMLAttributes<HTMLInputElement>
+
+export const VolumeSlider = ({ className, ...props }: VolumeSliderProps) => {
   const { player } = useSpotifyPlayer()
   // TODO: persist
   const [volume, setVolume] = React.useState(0.3)
@@ -15,20 +23,21 @@ export const VolumeSlider = (props: VolumeSliderProps) => {
   }, [player, volume])
 
   return (
-    <div {...props}>
-      <input
-        className="w-full"
-        type="range"
-        disabled={!player}
-        aria-label="Player volume"
-        min="0"
-        max="1"
-        step="0.01"
-        value={volume}
-        onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-          setVolume(parseFloat(event.target.value))
-        }
-      />
-    </div>
+    <input
+      className={cx(className, 'w-10')}
+      type="range"
+      disabled={!player}
+      aria-label="Player volume"
+      min="0"
+      max="1"
+      step="0.01"
+      value={volume}
+      onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+        setVolume(parseFloat(event.target.value))
+      }
+      orient="vertical"
+      style={{ WebkitAppearance: 'slider-vertical' }}
+      {...props}
+    />
   )
 }
