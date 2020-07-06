d313606841b819b141b296a709f7c28f6cd7c221
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2020-07-06 16:30:41 +0200

    bump deps, fix webdriverio chromium version issue

--- wdio.conf.js
+++ wdio.conf.js
@@ -101,7 +101,26 @@ exports.config = {
   // Services take over a specific job you don't want to take care of. They enhance
   // your test setup with almost no effort. Unlike plugins, they don't add new
   // commands. Instead, they hook themselves up into the test process.
-  services: ['chromedriver'],
+  services: [
+    [
+      'selenium-standalone',
+      {
+        logPath: 'logs',
+        installArgs: {
+          drivers: {
+            // releases: https://sites.google.com/a/chromium.org/chromedriver/downloads
+            chrome: { version: '83.0.4103.14' },
+          },
+        },
+        args: {
+          drivers: {
+            // releases: https://sites.google.com/a/chromium.org/chromedriver/downloads
+            chrome: { version: '83.0.4103.14' },
+          },
+        },
+      },
+    ],
+  ],
 
   // Framework you want to run your specs with.
   // The following are supported: Mocha, Jasmine, and Cucumber
