e43523d053fbd4f3c920b3ed6e58c19246a46880
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2020-09-01 09:45:17 +0200

    add endoint tests for auth routes

--- test/api/endpoints/auth_test.clj
+++ test/api/endpoints/auth_test.clj
@@ -1,10 +1,30 @@
 (ns api.endpoints.auth-test
   (:require [clojure.test :refer [deftest testing is]]
             [ring.mock.request :as mock]
+            [next.jdbc.sql :as sql]
+            [api.modules.spotify :as spotify]
             [api.endpoints.auth :as auth]))
 
+(def ^:private test-ctx {:config {:spotify {:client-id "client-id" :redirect-uri "https://redirect.uri"}}
+                         :ds "jdbc:postgresql:postgres?user=postgres&password=postgres"})
+
 (deftest auth-test
-  (testing "login"
-    (let [handler (auth/redirect-to-spotify {:config {:spotify {:client-id "client-id" :redirect-uri "https://redirect.uri"}}})
+  (testing "/auth/login"
+    (let [handler (auth/redirect-to-spotify test-ctx)
           result (handler (-> (mock/request :get "/auth/login")))]
-      (is (= {:status 307 :headers {"Location" "https://accounts.spotify.com/authorize?client_id=client-id&redirect_uri=https%3A%2F%2Fredirect.uri&response_type=code"}} result)))))
+      (is (= {:status 307 :headers {"Location" "https://accounts.spotify.com/authorize?client_id=client-id&redirect_uri=https%3A%2F%2Fredirect.uri&response_type=code"}} result))))
+  (testing "/auth/spotify-callback"
+    (with-redefs [spotify/trade-code-for-tokens (fn [_] {:access-token "access-token"
+                                                         :refresh-token "refresh-token"
+                                                         :expires-in 3600})
+                  spotify/invoke (fn [_ _] {:avatar "avatar" :id "id" :display-name "display-name"})]
+      (is (nil? (sql/get-by-id (:ds test-ctx) :users "id")))
+      (let [handler (auth/spotify-callback test-ctx)
+            result (handler (-> (mock/request :get "/auth/spotify-callback") (mock/query-string (str "code=" (apply str (repeat 191 "x"))))))]
+       ;; TODO: assert user in database 
+        (let [user (sql/get-by-id (:ds test-ctx) :users "id")]
+          (is (some? user))
+          (is (= "id" (:id user)))
+          (is (= "display-name" (:display-name user)))
+          (is (= "avatar" (:avatar user))))
+        (is (= 307 (:status result)))))))
