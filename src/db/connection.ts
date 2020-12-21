1257501e1d970f4353328278deb99207643b6935
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2020-12-21 13:25:24 +0100

    disable ssl since we use a local db in prod as well

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -3,14 +3,7 @@ import { Client } from 'pg'
 const connectionString = process.env.DATABASE_URL!
 
 export const makeClient = () => {
-  const client = new Client({
-    connectionString,
-    ssl: connectionString.includes('localhost')
-      ? undefined
-      : {
-          rejectUnauthorized: false,
-        },
-  })
+  const client = new Client({ connectionString })
   client.connect()
   return client
 }
