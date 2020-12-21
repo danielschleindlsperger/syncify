688026214c0b8cc4d3f7ba12fe7a6097a8b9d8b5
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2020-12-21 15:28:09 +0100

    fix CI steps for merge

--- .github/actions/ssh-tunnel/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -l
-
-set -eu
-
-echo "Opening SSH tunnel to ${INPUT_SSH_TUNNEL_DESTINATION}"
-sshpass -p "${INPUT_SSH_TUNNEL_PASSWORD}" sh -c 'ssh -fNT -o StrictHostKeyChecking=no -4 -L "${INPUT_SSH_TUNNEL_MAPPING}" "${INPUT_SSH_TUNNEL_DESTINATION}" -p "${INPUT_SSH_TUNNEL_PORT}" && sleep 5'
-
