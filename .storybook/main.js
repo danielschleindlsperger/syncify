eab977eca922b8a639bc8fe8814bbabdd6b62e19
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2021-01-03 17:52:00 +0100

    fix svgr component transformation in storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
   addons: [],
@@ -8,6 +10,12 @@ module.exports = {
       loader: require.resolve('babel-loader'),
     })
     config.resolve.extensions.push('.ts', '.tsx')
+
+    // Exclude svg icons from being loaded by the file-loader since we want to use the svgr loader
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test.test('.svg'))
+    const svgFolderPath = path.resolve(__dirname, '../src/icons')
+    fileLoaderRule.exclude = svgFolderPath
+
     return config
   },
 }
