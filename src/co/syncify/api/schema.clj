626f36a883b909c70a0b5f38ff0ad97be5a50edb
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2021-04-25 21:16:07 +0200

    add in-memory crux node

--- src/co/syncify/api/schema.clj
+++ src/co/syncify/api/schema.clj
@@ -2,6 +2,8 @@
   (:require [malli.core :as m]
             [malli.generator :as mg]))
 
+;; TODO: descriptions
+
 (def User [:map
            ;; TODO: how long are spotify ids?
            [:id [:string {:min 8 :max 32}]]
--- src/co/syncify/api/system.clj
+++ src/co/syncify/api/system.clj
@@ -2,16 +2,19 @@
   (:require [integrant.core :as ig]
             [taoensso.timbre :as log]
             [org.httpkit.server :refer [run-server]]
+            [crux.api :as crux]
             [co.syncify.api.config :refer [load-config]]
             [co.syncify.api.modules.spotify :as spotify :refer [create-spotify-client]]))
 
 (defn ->system-config [profile]
   {:http/server      {:config  (ig/ref :system/config)
                       :handler (ig/ref :http/app-handler)}
-   :http/app-handler {:spotify (ig/ref :spotify/client)
+   :http/app-handler {:db/crux (ig/ref :db/crux)
+                      :spotify (ig/ref :spotify/client)
                       :config  (ig/ref :system/config)}
    :system/config    {:profile profile}
-   :spotify/client   {:config (ig/ref :system/config)}})
+   :spotify/client   {:config (ig/ref :system/config)}
+   :db/crux          {}})
 
 ;;;;;;;;;;;;
 ;; Config ;;
@@ -34,13 +37,21 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; App handler (ring router) ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defmethod ig/init-key :http/app-handler [_ {:keys [config spotify]}]
+(defmethod ig/init-key :http/app-handler [_ system]
   (fn [req]
-    (spotify/request spotify :get-track {:id "1VbsSYNXKBpjPvqddk8zjs"})
-    {:status 200 :body "hello syncify"}))
+    (let [{:keys [:spotify :db/crux]} system]
+      (spotify/request spotify :get-track {:id "1VbsSYNXKBpjPvqddk8zjs"})
+      {:status 200 :body "hello syncify"})))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Spotify API client ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;
 (defmethod ig/init-key :spotify/client [_ {:keys [config]}]
   (create-spotify-client (:spotify config)))
+
+;;;;;;;;;;;;;
+;; Crux DB ;;
+;;;;;;;;;;;;;
+(defmethod ig/init-key :db/crux [_ {:keys []}]
+  ;; TODO: switch to something with persistence for local development as well as production
+  (crux/start-node {}))
