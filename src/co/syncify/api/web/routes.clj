62049acffa732ed22c23c94c5b62320d6bad27ce
Daniel Schleindlsperger <daniel@schleindlsperger.de>
2021-05-07 15:55:45 +0200

    ring default handler for 404 405 406 stauts codes

--- src/co/syncify/api/web/routes.clj
+++ src/co/syncify/api/web/routes.clj
@@ -1,25 +1,42 @@
 (ns co.syncify.api.web.routes
-  (:require [reitit.ring :as r]))
+  (:require [reitit.ring :as ring]
+            [reitit.ring.middleware.parameters :as parameters]
+            [reitit.dev.pretty :as pretty]))
 
-(def default-middleware [])
+(def default-middleware [;; query-params & form-params
+                         parameters/parameters-middleware
+                         ;; Multipart upload is missing here
+                         ])
 
-(defn home-handler [req] {:status 200
-                          :body   "hello!"})
+(defn home-handler [req]
+  (clojure.pprint/pprint (:query-params req))
+  {:status 200
+   :body   "hello!"})
 
-(defn foo-handler [req] {:status 200
-                         :body   "hello foo!"})
+(defn foo-handler [req]
+  (throw (ex-info "Nope" {:nope "really."})))
+
+(defn default-handler []
+  (ring/create-default-handler
+    {:not-found          (constantly {:status 404 :body "not found"})
+     :method-not-allowed (constantly {:status 405 :body "method not allowed"})
+     :not-acceptable     (constantly {:status 406 :body "not acceptable"})}))
 
 (defn routes []
   [["/" {:get home-handler}]
    ["/foo" {:get foo-handler}]])
 
-(def ->router #(r/router (routes)))
+(def ->router #(ring/router (routes)
+                            {:exception pretty/exception}))
 
 (defn app-handler [system]
   ;; TODO: Middleware that injects the system map into the request map
   ;; TODO: Util to retrieve a system component from the request map
   (let [router (->router)]
-    (r/ring-handler router {:middleware default-middleware})))
+    (ring/ring-handler
+      router
+      (default-handler)
+      {:middleware default-middleware})))
 
 (comment
   (def app (app-handler {}))
